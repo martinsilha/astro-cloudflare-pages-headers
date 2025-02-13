faa8ba0c4bdfe791f539abb65c86d974e2f5d305
Martin Silha <martysilha@gmail.com>
2025-02-12 18:50:08 -0600

    fix: update export statement in index.ts and enhance TypeScript configuration

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
-export { default as astroCloudflarePagesHeaders } from './src/integration';
+export { default } from './src/integration';
